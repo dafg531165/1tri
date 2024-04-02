173281d4cae47e5c368cb682983ba9a55330e97e
dafg531165 <107404931+dafg531165@users.noreply.github.com>
2024-04-02 10:16:47 -0300

    dad

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
-const texto = document.querySelectorAll(".aba-conteudo")
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0 < botoes.lenght; i++){
+for (let i = 0; i< botoes.lenght; i++){
   botoes[i].onlick = function (){
     for (let j = 0; j < botoes.lenght; j++){
       botoes[j].classList.remove("ativo");
       textos[j].classList.remove("ativo");
     }
-    botoes[i].classList.add("ativo")
-    textos[i].classList.add("ativo")
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
   }
 }
