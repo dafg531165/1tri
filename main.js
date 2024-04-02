0a59613a06719c9e5d06104dae94a840bf42f159
dafg531165 <107404931+dafg531165@users.noreply.github.com>
2024-04-02 09:15:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
-
+const botoes = document.querySelectorAll(".aba-conteudo")
 for (let i = 0 < botoes.lenght; i++){
   botoes[i].onlick = function (){
     for (let j = 0; j < botoes.lenght; j++){
       botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
     }
     botoes[i].classList.add("ativo")
-
+    textos[i].classList.add("ativo")
   }
 }
