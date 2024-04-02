a19d375439f93f8d368cce8699ae020960300a51
dafg531165 <107404931+dafg531165@users.noreply.github.com>
2024-04-02 10:20:36 -0300

    d

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i<botoes.lenght; i++){
+for (let i = 0; i<botoes.length; i++){
   botoes[i].onclick = function (){
-    for (let j = 0; j<botoes.lenght; j++){
+    for (let j = 0; j<botoes.length; j++){
       botoes[j].classList.remove("ativo");
       textos[j].classList.remove("ativo");
     }
