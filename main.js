255fd235312af735054079932724cf3f9e6eab51
dafg531165 <107404931+dafg531165@users.noreply.github.com>
2024-04-02 10:19:52 -0300

    EWER

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i<botoes.lenght; i++){
-  botoes[i].onlick = function (){
+  botoes[i].onclick = function (){
     for (let j = 0; j<botoes.lenght; j++){
       botoes[j].classList.remove("ativo");
       textos[j].classList.remove("ativo");
