395f7aaa9af29dff61d31cda295b281a3563f7a4
dafg531165 <107404931+dafg531165@users.noreply.github.com>
2024-05-10 10:27:55 -0300

    ddf

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ for (let i = 0; i<botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo2 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo3 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo4 = new Date("2024-12-28T23:59:59");
+const tempoObjetivo1 = new Date("2024-12-14T23:59:59");
+const tempoObjetivo2 = new Date("2024-11-27T23:59:59");
+const tempoObjetivo3 = new Date("2025-12-31T23:59:59");
+const tempoObjetivo4 = new Date("2024-12-04T23:59:59");
 
 const tempos = [
     tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
